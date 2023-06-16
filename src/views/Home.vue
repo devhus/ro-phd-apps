d5a4f1f78b9037949d4f727c0ab3097e78a7a945
devhus <devhus01@gmail.com>
2023-06-16 20:29:14 +0300

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,38 +2,36 @@
 </script>
 
 <template>
-  <div class="p-5">
-    <div class="row g-3">
-      <div class="col">
-        <RouterLink :to="{ name: 'app-1-1' }">
-          App 1
-        </RouterLink>
-      </div>
-      <div class="col">
-        <RouterLink :to="{ name: 'app-2-1' }">
-          App 2
-        </RouterLink>
-      </div>
-      <div class="col">
-        <RouterLink :to="{ name: 'app-3-1' }">
-          App 3
-        </RouterLink>
-      </div>
-      <div class="col">
-        <RouterLink :to="{ name: 'app-4-1' }">
-          App 4
-        </RouterLink>
-      </div>
-      <div class="col">
-        <RouterLink :to="{ name: 'app-5-1' }">
-          App 5
-        </RouterLink>
-      </div>
-      <div class="col">
-        <RouterLink :to="{ name: 'app-6-1', params: { level: 1 } }">
-          App 6
-        </RouterLink>
-      </div>
+  <div class="p-5 d-flex flex-column gap-3">
+    <div class="col">
+      <RouterLink class="btn btn-primary" :to="{ name: 'app-1-1' }">
+        App 1
+      </RouterLink>
+    </div>
+    <div class="col">
+      <RouterLink class="btn btn-primary" :to="{ name: 'app-2-1' }">
+        App 2
+      </RouterLink>
+    </div>
+    <div class="col">
+      <RouterLink class="btn btn-primary" :to="{ name: 'app-3-1' }">
+        App 3
+      </RouterLink>
+    </div>
+    <div class="col">
+      <RouterLink class="btn btn-primary" :to="{ name: 'app-4-1' }">
+        App 4
+      </RouterLink>
+    </div>
+    <div class="col">
+      <RouterLink class="btn btn-primary" :to="{ name: 'app-5-1' }">
+        App 5
+      </RouterLink>
+    </div>
+    <div class="col">
+      <RouterLink class="btn btn-primary" :to="{ name: 'app-6-1', params: { level: 1 } }">
+        App 6
+      </RouterLink>
     </div>
   </div>
 </template>
